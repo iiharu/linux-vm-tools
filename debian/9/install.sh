2c82430e74ec0bba210fccb1df7011454a564eda
iiharu <iiharu@alumni.tus.ac.jp>
2019-05-20 11:31:30 +0900

    debian/9/install.sh: add debian 9 install.sh

--- debian/9/install.sh
+++ debian/9/install.sh
@@ -1,5 +1,95 @@
 #!/bin/bash
 
 #
-# This script is for Debian 9 Stretch to download install XRDP+XORGXRDP.
+# This script is for Debian 9 Stretch to install XRDP+XORGXRDP.
 #
+
+###############################################################################
+# Update our machine to the latest code if we need to.
+#
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo 'This script must be run with root privileges' >&2
+    exit 1
+fi
+
+cat << EOF > /etc/apt/sources.list.d/backports.list
+deb https://deb.debian.org/debian stretch-backports main contrib non-free
+# deb-src https://deb.debian.org/debian stretch-backports main contrib non-free
+EOF
+
+apt update && apt upgrade -y
+
+if [ -f /var/run/reboot-required ]; then
+    echo "A reboot is required in order to proceed with the install." >&2
+    echo "Please reboot and re-run this script to finish the install." >&2
+    exit 1
+fi
+
+###############################################################################
+# Install XRDP
+#
+
+# Install hv_kvp utils
+apt install -y -t=stretch-backports linux-image-amd64 linux-headers-amd64 hyperv-daemons
+
+# Install xrdp
+apt install -y -t=stretch-backports xrdp
+
+###############################################################################
+# Configure XRDP
+#
+
+systemctl enable xrdp
+systemctl enable xrdp-sesman
+
+cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.orig
+# use vsock transport.
+sed -i_orig -e 's/use_vsock=false/use_vsock=true/g' /etc/xrdp/xrdp.ini
+# use rdp security.
+sed -i -e 's/security_layer=negotiate/security_layer=rdp/g' /etc/xrdp/xrdp.ini
+# remove encryption validation.
+sed -i -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
+# disable bitmap compression since its local its much faster
+sed -i -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
+sed -n -e 's/max_bpp=32/max_bpp=24/g' /etc/xrdp/xrdp.ini
+
+cp /etc/xrdp/sesman.ini /etc/xrdp/sesman.ini.orig
+# rename the redirected drives to 'shared-drives'
+sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc/xrdp/sesman.ini
+
+# Changed the allowed_users
+echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
+
+# Blacklist the vmw module
+if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ]; then
+cat >> /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf <<EOF
+blacklist vmw_vsock_vmci_transport
+EOF
+fi
+
+# Ensure hv_sock gets loaded
+if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
+	echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
+fi
+
+# Configure the policy xrdp session
+mkdir -p /etc/polkit-1/localauthority/50-local.d/
+cat > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+
+# reconfigure the service
+systemctl daemon-reload
+
+#
+# End XRDP
+###############################################################################
+
+echo "Install is complete."
+echo "Reboot your machine to begin using XRDP."
